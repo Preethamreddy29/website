f11a936108df812bfc3169f1ffd85c3b2855fc07
Preetham reddy <preethamreddy2429@gmail.com>
2024-08-04 15:48:46 +0530

    Updates

---
+++ precache-manifest.1847680962dac81f2c425aaa006e28d7.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b353ea5f81b81476856647a9ffa2571",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "4730a747941c4776c338",
+    "url": "/website/static/css/2.04b36542.chunk.css"
+  },
+  {
+    "revision": "a3dbe2c53bae06992851",
+    "url": "/website/static/css/main.1a028591.chunk.css"
+  },
+  {
+    "revision": "4730a747941c4776c338",
+    "url": "/website/static/js/2.44c259e8.chunk.js"
+  },
+  {
+    "revision": "a3dbe2c53bae06992851",
+    "url": "/website/static/js/main.de990453.chunk.js"
+  },
+  {
+    "revision": "d15da1c5dbbceb93f3a7",
+    "url": "/website/static/js/runtime~main.de462641.js"
+  },
+  {
+    "revision": "39bdec48278b05f818633686867f5079",
+    "url": "/website/static/media/backdog.39bdec48.jpg"
+  },
+  {
+    "revision": "3408f24200c5ea62b2021d0181aa19de",
+    "url": "/website/static/media/caroback.3408f242.png"
+  },
+  {
+    "revision": "d39546272224032c50df9d0a5677c15a",
+    "url": "/website/static/media/dog.d3954627.jpg"
+  },
+  {
+    "revision": "06f5faa4b242fe18f5b1a56dd26df15e",
+    "url": "/website/static/media/dogbath.06f5faa4.jpg"
+  },
+  {
+    "revision": "6d10ceb8382c7680e63eb00e4fbc858a",
+    "url": "/website/static/media/dogear.6d10ceb8.jpg"
+  },
+  {
+    "revision": "5c7c737fe84a721531706e73ed637bee",
+    "url": "/website/static/media/doghair.5c7c737f.jpg"
+  },
+  {
+    "revision": "e8f50d7def8c95eeb375f6b35c4ef45e",
+    "url": "/website/static/media/dognail.e8f50d7d.jpg"
+  },
+  {
+    "revision": "18f363a2c77551bbacc4c7b17bed5eb3",
+    "url": "/website/static/media/dogpaw.18f363a2.jpg"
+  },
+  {
+    "revision": "b103802ded80f31259dace6cd133101e",
+    "url": "/website/static/media/logsinback.b103802d.jpg"
+  },
+  {
+    "revision": "b8faab6aa5ec00b799ede7c62a21b96b",
+    "url": "/website/static/media/outdog1.b8faab6a.png"
+  },
+  {
+    "revision": "d7667534fb923fe4ba9c47df8a8ccfc6",
+    "url": "/website/static/media/outdog2.d7667534.png"
+  },
+  {
+    "revision": "ee6f778154a2308673083695123fc61e",
+    "url": "/website/static/media/outdog3.ee6f7781.png"
+  },
+  {
+    "revision": "36791025deae47bdc4444f5c7e88998f",
+    "url": "/website/static/media/outdog4.36791025.png"
+  },
+  {
+    "revision": "f5f0b49ce5a9425e69efbf4d60ef1314",
+    "url": "/website/static/media/rev1.f5f0b49c.jpg"
+  },
+  {
+    "revision": "0f0fde11ce83f6aac96b975fa196513b",
+    "url": "/website/static/media/rev10.0f0fde11.jpg"
+  },
+  {
+    "revision": "00e7207688bb485d0276ba965b83e61a",
+    "url": "/website/static/media/rev11.00e72076.jpg"
+  },
+  {
+    "revision": "1d491a13fe9bfa540269f519600e27b9",
+    "url": "/website/static/media/rev12.1d491a13.jpg"
+  },
+  {
+    "revision": "e1b1ed8281c1047e2cdf75d2f8b1f861",
+    "url": "/website/static/media/rev13.e1b1ed82.jpg"
+  },
+  {
+    "revision": "60b3d46dbcc980b2bce11e54d0c42141",
+    "url": "/website/static/media/rev14.60b3d46d.jpg"
+  },
+  {
+    "revision": "2ed454a0776f1e0332e36702564ef668",
+    "url": "/website/static/media/rev15.2ed454a0.jpg"
+  },
+  {
+    "revision": "3878c93bd33b014351ca4a616532fc22",
+    "url": "/website/static/media/rev2.3878c93b.jpg"
+  },
+  {
+    "revision": "b191f75650d6c07a9f25114578c28c29",
+    "url": "/website/static/media/rev3.b191f756.jpg"
+  },
+  {
+    "revision": "264e83615757afa15b3a0068fbdd1a9b",
+    "url": "/website/static/media/rev4.264e8361.jpg"
+  },
+  {
+    "revision": "4d431992021f2502dccacf07d719a845",
+    "url": "/website/static/media/rev5.4d431992.jpg"
+  },
+  {
+    "revision": "1c69946cfac35cd9d883cbd141061eee",
+    "url": "/website/static/media/rev6.1c69946c.jpg"
+  },
+  {
+    "revision": "e2c3135bdc68c773d03ca243cd9e345f",
+    "url": "/website/static/media/rev7.e2c3135b.jpg"
+  },
+  {
+    "revision": "7ee76ef42249e0c1403af214f3c21b2e",
+    "url": "/website/static/media/rev8.7ee76ef4.jpg"
+  },
+  {
+    "revision": "8c0c9e488f474b8f745bbc54a5d0fd88",
+    "url": "/website/static/media/rev9.8c0c9e48.jpg"
+  }
+]);
